5a03c9f9d8533e2eea993b370d438af80895fdf3
siobhan <sr@saibhreas.com>
2021-05-05 15:10:39 -0400

    just a psuedo code, start of project.

---
+++ assest/js/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+
+
+var specialChars=[","  "#"];
+
+//
+var numbArray = [];
+
+//lower case Array
+
+var lowerCase =[];
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
